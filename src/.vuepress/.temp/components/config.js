4e04f17a6fb95b74af3b24d0fc0684a2c3252516
李超凡 <lichaofan@newland.com.cn>
2024-01-31 14:22:47 +0800

    项目初始化

---
+++ src/.vuepress/.temp/components/config.js
@@ -0,0 +1,43 @@
+import { defineClientConfig } from "vuepress/client";
+import { hasGlobalComponent } from "D:/工作空间/person-program/cube-interview/node_modules/vuepress-shared/lib/client/index.js";
+import { h } from "vue";
+
+import { useScriptTag } from "D:/工作空间/person-program/cube-interview/node_modules/@vueuse/core/index.mjs";
+import FontIcon from "D:/工作空间/person-program/cube-interview/node_modules/vuepress-plugin-components/lib/client/components/FontIcon.js";
+import Badge from "D:/工作空间/person-program/cube-interview/node_modules/vuepress-plugin-components/lib/client/components/Badge.js";
+import VPCard from "D:/工作空间/person-program/cube-interview/node_modules/vuepress-plugin-components/lib/client/components/VPCard.js";
+import BackToTop from "D:/工作空间/person-program/cube-interview/node_modules/vuepress-plugin-components/lib/client/components/BackToTop.js";
+
+import "D:/工作空间/person-program/cube-interview/node_modules/vuepress-plugin-components/lib/client/styles/sr-only.scss";
+
+export default defineClientConfig({
+  enhance: ({ app }) => {
+    if(!hasGlobalComponent("FontIcon")) app.component("FontIcon", FontIcon);
+    if(!hasGlobalComponent("Badge")) app.component("Badge", Badge);
+    if(!hasGlobalComponent("VPCard")) app.component("VPCard", VPCard);
+    
+  },
+  setup: () => {
+    useScriptTag(
+  `https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/js/brands.min.js`,
+  () => {},
+  { attrs: { "data-auto-replace-svg": "nest" } }
+);
+
+    useScriptTag(
+  `https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/js/solid.min.js`,
+  () => {},
+  { attrs: { "data-auto-replace-svg": "nest" } }
+);
+
+    useScriptTag(
+  `https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6/js/fontawesome.min.js`,
+  () => {},
+  { attrs: { "data-auto-replace-svg": "nest" } }
+);
+
+  },
+  rootComponents: [
+    () => h(BackToTop, {}),
+  ],
+});
